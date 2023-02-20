a9cd4de8a53765206c9988e1081d9be7101f2e9f
KehindeAramide <kenned2k4@yahoo.com>
2023-02-20 03:46:22 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,18 +8,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = 0; n <= 9; n++)
 	{
-
-		if (n == 9)
 			putchar (n + '0');
-		else
-		{	putchar (',');
+			putchar (',');
 			putchar (' ');
-			putchar (n + '0');
-		}
+
+
 	}
 
-
+	putchar ('9');
 	return (0);
 }
