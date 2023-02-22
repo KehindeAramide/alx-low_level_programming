3776a5c2f916c3f8ed8079815f6afbf0f3264694
KehindeAramide <kenned2k4@yahoo.com>
2023-02-21 19:33:09 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,22 @@
 #include "main.h"
 /**
-* print_last_digit - this print the last digit
-* @i: function parameter
-* Return: k
-*/
+ * print_last_digit-prints the last digit of an interger.
+ * @a:number to compute last digit.
+ * Return:last digit.
+ */
 
-int print_last_digit(int i)
+
+
+int print_last_digit(int a)
 {
-	int k;
+	int last_digit;
 
-	k = i % 10;
-	if (i < '0')
+	last_digit = a %  10;
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+	_putchar(last_digit + '0');
+	return (last_digit);
 
-		k = -k;
-	_putchar (k + '0');
-	return (k);
 }
