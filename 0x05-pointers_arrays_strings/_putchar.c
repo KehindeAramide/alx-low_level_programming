ff603a7ba2499999858338fbcb72c350886299e4
KehindeAramide <kenned2k4@yahoo.com>
2023-02-27 12:41:33 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "unistd.h"
+
+/**
+ * _putchar - this writes the character c to stdout
+ * @c: character to be publish
+ * Return: on success 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
