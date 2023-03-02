6fefaf25c8d2dd3e2c2413b0c6acb3b572bd9bb9
KehindeAramide <kenned2k4@yahoo.com>
2023-03-02 11:51:24 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,19 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0 ; i < n ; i++)
+	i = 0;
+
+	for (i < n ; &&src[i] != '\0')
+	{
+		dest[i] = src[i]
+		i++;
+	}
 		dest[i] = src[i];
+
+	while (j < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
