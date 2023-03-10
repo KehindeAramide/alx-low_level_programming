372f9238bba1b693d5c463380f2ebe24c184bfd0
KehindeAramide <kenned2k4@yahoo.com>
2023-03-10 06:59:41 -0800

    newtask 0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,15 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-void _puts(char *str)
-
-#include<stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * _puts - this write string to standard output
+ * @s: input
  */
-int main(void)
+
+void _puts(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts(s);
 }
