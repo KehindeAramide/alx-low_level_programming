9e6914a5e3ecee9352d62be5e9136e1df9f2e25d
KehindeAramide <kenned2k4@yahoo.com>
2023-03-15 00:49:56 -0700

    advance task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,31 +12,32 @@ void create_word(char **, char *, int, int, int);
 */
 char **strtow(char *str)
 {
+
 	int i, flag, len;
 	char **words;
 
-	for (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
+	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
 		return (NULL);
+	i = flag = len = 0;
 
-		i = flag = len = 0;
 	while (str[i])
 	{
-	if (flag == 0 && str[i] != ' ')
-	flag = 1;
-	if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		if (flag == 0 && str[i] != ' ')
+			flag = 1;
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
 	{
-		flag = 0;
-		len++;
+	flag = 0;
+	len++;
 	}
-	i++;
-}
+		i++;
+	}
+
 	len += flag == 1 ? 1 : 0;
 	if (len == 0)
-	return (NULL);
-
-		words = (char **)malloc(sizeof(char *) * (len + 1));
+		return (NULL);
+	words = (char **)malloc(sizeof(char *) * (len + 1));
 	if (words == NULL)
-	return (NULL);
+		return (NULL);
 
 	util(words, str);
 	words[len] = NULL;
@@ -53,14 +54,13 @@ void util(char **words, char *str)
 	int i, j, start, flag;
 
 	i = j = flag = 0;
-
 	while (str[i])
 	{
-		if (flag == 0 && str[i] != ' ')
-		{
-			start = i;
-			flag = 1;
-		}
+	if (flag == 0 && str[i] != ' ')
+	{
+	start = i;
+	flag = 1;
+	}
 	if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
 	{
 		create_word(words, str, start, i, j);
@@ -90,7 +90,8 @@ void create_word(char **words, char *str, int start, int end, int index)
 
 	words[index] = (char *)malloc(sizeof(char) * (i + 1));
 
+
 	for (j = 0; start < end; start++, j++)
-		words[index][j] = str[start];
+	words[index][j] = str[start];
 	words[index][j] = '\0';
 }
